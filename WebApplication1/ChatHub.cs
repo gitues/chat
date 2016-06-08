22e8b06cb37fcc9dd8e71dd1631e3745d2b5b9d7
Alex Rodriguez <RODRIA13@DC_PLSS>
2016-06-08 17:31:42 -0600

    first commit

---
+++ WebApplication1/ChatHub.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.Threading;
+
+using WhatsAppApi;
+
+namespace WebApplication1
+{
+    public class ChatHub : Hub
+    {
+        static WhatsApp wa;
+
+
+        public void send(String to, string msg)
+        {
+            if (wa.ConnectionStatus == ApiBase.CONNECTION_STATUS.LOGGEDIN)
+            {
+                wa.SendMessage(to,msg);
+                Clients.All.notifyMessage(string.Format ("{0}: {1}",to,msg));
+            }
+
+        }
+
+        public void login(string phoneNumber, string pass)
+        {
+            Thread thread = new Thread(t =>
+                {
+                    wa = new WhatsApp(phoneNumber, pass, phoneNumber, true);
+                    wa.OnConnectSuccess += () =>
+                        {
+                            Clients.All.notifyMessage("Conectado ...");
+
+                            wa.OnLoginSuccess += (phone, data) =>
+                                {
+                                    Clients.All.notifyMessage("Logueo exitoso ...");
+                                };
+
+                            wa.OnLoginFailed += (data) =>
+                                {
+                                    Clients.All.notifyMessage(string.Format("Fallo en login {0}", data));
+                                };
+                            wa.Login();
+                        };
+
+                    wa.OnConnectFailed += (ex) =>
+                    {
+                        Clients.All.notifyMessage(string.Format("Fallo de conexión {0}", ex.StackTrace));
+                    };
+
+                    wa.Connect();
+                }) {IsBackground = true  };
+            thread.Start(); 
+        }   
+    }
+}
---
+++ WebApplication1/Startup.cs
@@ -0,0 +1,18 @@
+﻿using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1
+{
+    public class Startup
+    {
+
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+
+    }
+}
